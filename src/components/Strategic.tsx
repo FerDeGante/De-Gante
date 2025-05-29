26a487c4bce19107f5a0d705f1cbb9544dbeff5c
FerDeGante <150310894+FerDeGante@users.noreply.github.com>
2025-05-29 12:24:17 -0600

    Improve Strategic section responsiveness

--- src/components/Strategic.tsx
+++ src/components/Strategic.tsx
@@ -10,7 +10,14 @@ export default function Strategic() {
         <p className="strategic-subtitle"> <b>Mi proyecto más destacado en mi vida profesional.</b>Iniciando con licencias de funcionamiento sin venta de alcohol, con capas de ortofotografía y vista de los predios, 
         por primera vez el Ayuntamiento de Cuernavaca cuenta con una plataforma de gestión de territorio y un trámite en línea de punta a punta.
         <b>¿Adaptamos la plataforma en tu ciudad?</b></p>
-        <a href="https://visorurbano.cuernavaca.gob.mx/inicio" className="strategic-button">Conoce este proyecto</a>
+        <a
+          href="https://visorurbano.cuernavaca.gob.mx/inicio"
+          className="strategic-button"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Conoce este proyecto
+        </a>
       </div>
       <div className="strategic-right">
         <Image
